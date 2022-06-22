f60e4b9d9aef3a1e512e5666e2a0939b871e990a
IkeshiroHirono <ikeshiro@azure-plus.co.jp>
2022-06-22 10:26:18 +0900

    docusaurus.config.js修正

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('C:\\Github\\testdocs\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Github\\testdocs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Github\\testdocs\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('C:\\Github\\testdocs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Github\\testdocs\\src\\css\\custom.css'),
+];
